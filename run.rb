420690feae97b8b6242b2b5796e2713fba6cc31b
Phillip Oertel <me@phillipoertel.com>
2014-01-28 14:08:22 +0100

    Allow passing an alternative file for the speed test; log the file name so the log can be split.

--- run.rb
+++ run.rb
@@ -1,6 +1,8 @@
 require 'logger'
 require_relative 'file_downloader'
-url = 'https://androidnetworktester.googlecode.com/files/1mb.txt'
+
+url = ARGV[0] || 'https://androidnetworktester.googlecode.com/files/1mb.txt'
+puts "Using test file: #{url}"
 
 logger = Logger.new('duration.log')
 logger.level = Logger::INFO
@@ -15,7 +17,7 @@ end
 
 loop do
   duration = measure { Torial::FileDownloader.download(url) }
-  log_string = "#{Time.now}: #{duration}"
+  log_string = "#{Time.now}: #{duration} : #{url}"
   if duration > 3
     1.times { `afplay /System/Library/Sounds/Blow.aiff` }
   end
